753e33df9344c6732b17945d78c39ad7af712ab7
DelTechs01 <bleamalika@gmail.com>
2025-04-11 22:27:46 +0300

    Add table definition for publishers in bookstore database

--- answer.sql
+++ answer.sql
@@ -1,3 +1,11 @@
 CREATE DATABASE bookstore;
 USE bookstore;
 
+-- Stores information about publishers, including their name and contact details.
+CREATE TABLE publisher (
+    publisher_id INT AUTO_INCREMENT PRIMARY KEY,
+    publisher_name VARCHAR(255) NOT NULL,
+    contact_email VARCHAR(255),
+    contact_phone VARCHAR(20)
+);
+
